35a22f01d6b1320f019cf5b5fe4b33c3c0ad4ebc
GuerraAna <ana.guerrads@hotmail.com>
2021-04-24 19:44:00 -0300

    Second Commit: Esqueci a Senha Layout

---
+++ android/app/src/main/java/br/com/linkyoung/EsqueciSenhaActivity.kt
@@ -0,0 +1,11 @@
+package br.com.linkyoung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EsqueciSenhaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_esqueci_senha)
+    }
+}
