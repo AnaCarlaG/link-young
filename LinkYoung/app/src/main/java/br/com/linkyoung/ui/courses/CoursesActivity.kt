35c4b8a250f9391c64ed7eca5f48950656c97cd9
GuerraAna <ana.guerrads@hotmail.com>
2021-04-25 03:23:02 -0300

    New Commit: layout de perfil e video

---
+++ LinkYoung/app/src/main/java/br/com/linkyoung/ui/courses/CoursesActivity.kt
@@ -0,0 +1,12 @@
+package br.com.linkyoung.ui.courses
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.linkyoung.R
+
+class CoursesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_courses)
+    }
+}
